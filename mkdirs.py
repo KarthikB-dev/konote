3bc2ff6fe04aba3474395a84df1b0d8163fdc631
KarthikB-dev <saberlight35@gmail.com>
2021-03-11 15:25:04 -0800

    Stylistic improvements in mkdirs.py

--- mkdirs.py
+++ mkdirs.py
@@ -1,29 +1,38 @@
 import os
 
+new_dir_path = input("In what directory do you wish to store all your notes?")
 def create_dirs(num, dir_type):
+    #17576 is the maxium supported chapter amount because 3 letters are used 
+    #for ordering the chapters
     if num == 0:
         #nothing needs to be done
         return
     if num > 17576:
         print ("Error: Exceeds number of accepted directories. ")
-        #createChapters(input("Please enter a valid number between 1 and 17576 inclusive."), dir_type)
+        #createChapters(input("Please enter a valid number between 1 and 17576 inclusive."), 
+        # dir_type)
         return
-    #if an acceptable chapter amount has been entered
-    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    #if an acceptable chapter or chapter section amount has been entered
+    letters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ]
     #generate the appropriate set of chapters
     i = 1
     #l1 is for the first letter, l2 for the second, l3 for the third
     #example chapter name: ADC_Chapter_17_Cell_Biology
+    #the letters at the start keep all the chapters listed in the proper order
     for l1 in letters:
         for l2 in letters:
             for l3 in letters:
                 dirName = l1 + l2 + l3 + '_'  + str(i) + "_"
-                dirDescription =  input("What is the description of " + '_' + dir_type + '_' + str(i) + "? ")
-                #replaces all spaces with underscores to avoid problems with escape characters
+                dirDescription =  input("What is the description of " + 
+                '_' + dir_type + '_' + str(i) + "? ")
+                #deals with problems with escape characters
                 dirDescription = dirDescription.replace(' ', '_')
                 dirName = dirName + dirDescription
                 #TODO implement some form of input handling
-                os.system("mkdir " + dirName)
+                os.system("mkdir " + new_dir_path + '/' + dirName)
                 i += 1
                 #If you've already made enough chapters or chapter sections
                 if i > num:
@@ -35,4 +44,4 @@ while dir_type != 'chapter' and dir_type != 'chapter_section':
     print("Do you want to make a chapter or chapter section? ")
     dir_type = input("Enter 'chapter' or 'chapter_section' ")
 
-create_dirs(int(input("How many chapters do you want to make? ")), dir_type)
+create_dirs(int(input("How many directories do you want to make? ")), dir_type)
