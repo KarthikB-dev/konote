c6ce1fdfd17b7b0dd34a691d75ce614581b81e0e
KarthikB-dev <saberlight35@gmail.com>
2021-03-11 15:02:55 -0800

    Removed duplicate file chapters.py

--- toggle.py
+++ toggle.py
@@ -5,6 +5,9 @@ def hide_dir(dir_name):
     os.system('mv ' + dir_name + hide_name)
 def reveal_hidden():
     os.system('ls - a ')
+def display_current_dir():
+    os.system('tree $(pwd)')
 
 #testing of the program
-hide_dir(input("What is the name of the directory you want to hide?"))
+hide_dir(input("What is the name of the directory you want to hide? "))
+display_current_dir()
