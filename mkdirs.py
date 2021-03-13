66b04ff4b53e686211cd8507ca325341283ba5fe
KarthikB-dev <saberlight35@gmail.com>
2021-03-12 23:12:50 -0800

    Removed outdated comments

--- mkdirs.py
+++ mkdirs.py
@@ -15,11 +15,12 @@ ic(get_zero_num(27))
 ic(get_zero_num(987))
 ic(get_zero_num(3080))
 
+#num is the number of new directories to be made
+#dir_type is the type of directory, either chapter or chapter section
+#new_dir_path is the file path to the directory where these new subdirectories
+#are to be made
 def create_dirs(num, dir_type, new_dir_path):
-    #TODO switch to a chapter/chapter_section followed by a number, with 0 placeholder
-    #eg. instead of AAC_chapter3_cell_biology -> 003_chapter3_cell_biology
     #9999 is the maxium supported chapter amount because 4 digits are used 
-    #for ordering the chapters
     if num == 0:
         #nothing needs to be done
         return
@@ -29,15 +30,8 @@ def create_dirs(num, dir_type, new_dir_path):
         # dir_type)
         return
     #if an acceptable chapter or chapter section amount has been entered
-    """
-    letters = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    ]
-    """
-    #generate the appropriate set of chapters
     i = int(input("What is the starting number for the chapter or chapter section? "))
-    while i <= num:
+    while i < num + i:
         dirName = dir_type + '_' + get_zero_num(i) + "_"
         dirDescription =  input("What is the description of " + 
         '_' + dir_type + '_' + str(i) + "? ")
