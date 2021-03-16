50b6a2e014adcaa5ad8a7b16923758a281de42db
KarthikB-dev <saberlight35@gmail.com>
2021-03-15 17:02:18 -0700

    get_display_name provides the proper dir name for display

--- toggle.py
+++ toggle.py
@@ -1,5 +1,5 @@
 import os
-
+from pathlib import path
 def hide_dir(dir_name):
     hide_name = '.' + dir_name
     os.system('mv ' + dir_name + hide_name)
@@ -13,7 +13,15 @@ def reveal_hidden():
     os.system('tree -a ')
 def display_current_dir():
     os.system('tree $(pwd)')
+#get_display_name removes the directory header
+def get_display_name(name: str) -> str:
+    if (name == 'description.txt'):
+        return None
+    return name[name.find('_') + 1:]
+#testing get_display_name
+print(get_display_name("cs0002_The_main_types_of_malware"))
+print(get_display_name("description.txt"))
 
 #testing of the program
-hide_dir(input("What is the name of the directory you want to hide? "))
-display_current_dir()
+#hide_dir(input("What is the name of the directory you want to hide? "))
+#display_current_dir()
