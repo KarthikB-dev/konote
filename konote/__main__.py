890cf55141ca2edb370151d08bf9622620dccfb6
KarthikB-dev <saberlight35@gmail.com>
2021-04-03 16:53:01 -0700

    Added JSON dump functionality

--- konote/__main__.py
+++ konote/__main__.py
@@ -1,7 +1,12 @@
 import os
+import ujson
+
 def input_num_tabs(file_name):
     with open file_name as f:
         lines = f.readlines()
 if __name__ == "__main__":
     num_tabs = input_num_tabs(input.txt)
-
+    #TODO complete input file processing
+    notes = {}
+    with open("notes.json", "w+") as fout:
+        ujson.dump(note_json, fout)
