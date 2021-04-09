249c81551b69f2de774e0e92c50b90ed18ff638d
KarthikB-dev <saberlight35@gmail.com>
2021-04-08 20:39:00 -0700

    Removed input num tabs

--- konote/input.py
+++ konote/input.py
@@ -1,13 +1,6 @@
-from Pathlib import Path
+from pathlib import Path
 import ujson
 
-#returns the number of tabs found at the beginning of each line
-def input_num_tabs(file_name):
-    with open(file_name, 'r') as fin:
-        lines = fin.read()
-        print(lines)
-        #TODO change this!
-num_tabs = input_num_tabs(input.txt)
 '''TODO complete input file processing
 The desired functionality is to add one level of nesting
 for every indent
@@ -50,5 +43,9 @@ to be stored in a JSON.
 '''
 #TODO make notes have the desired dictionary structure
 notes = {}
+
+notes_path = Path.home() / "Documents" / "konote_notes" / "notes.txt"
+with open(notes_path, 'r') as fin:
+    lines = fin.readlines()
 with open("notes.json", "w+") as fout:
     ujson.dump(notes, fout)
