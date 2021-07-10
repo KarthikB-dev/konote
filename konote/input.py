ef7adb579cd553330f53814ac5334ea840b20056
KarthikB-dev <saberlight35@gmail.com>
2021-07-09 22:51:56 -0700

    Add argparse input

--- konote/input.py
+++ konote/input.py
@@ -1,74 +1,21 @@
 from pathlib import Path
-import ujson
+import argparse
+import yaml
+# from icecream import ic
 
-'''TODO complete input file processing
-The desired functionality is to add one level of nesting
-for every indent
-for example, if we had the following:
-Programming notes
-    Algorithms
-        Binary Search
-        Sequential Search
-    Data structures
-        Collections
-            Set
-            ArrayList
-            LinkedList
-            Queue
-        Maps
-            HashMap
-            TreeMap
-We would have
-"Programming notes": {
-    "Algorithms": {
-       "Binary Search",
-       "Sequential Search"
-    },
-    "Data Structures": {
-        "Collections":{
-           "Set",
-           "ArrayList",
-           "LinkedList",
-           "Queue"
-        },
-        "Maps"
-        {
-           "HashMap",
-           TreeMap"
-        }
-    }
-}
+# import ujson
 
-to be stored in a JSON.
-'''
-#TODO make notes have the desired dictionary structure
-notes = dict()
-
-def get_num_tabs(line):
-    num_tabs = 0
-    for char in line:
-        if char == '\t':
-            num_tabs += 1
-        else:
-            break
-    return num_tabs
-
-#checks that indentation is done correctly in the notes
-def proper_format(lines):
-    prev_num = get_num_tabs(lines[0])
-    line_num = 0
-    for line in lines:
-        if get_num_tabs(line) > prev_num + 1:
-            return "Incorrect formatting on line " + line_num
-        prev_num = get_num_tabs(line)
-        line_num += 1
-
-notes_path = Path.home() / "Documents" / "konote_notes" / "notes.txt"
-with open(notes_path, 'r') as fin:
-    lines = fin.readlines()
-    print(lines)
-    for line in lines:
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "note_type", type=int, help="1 for quick todo, 2 for project, 3 for long term goal"
+)
+parser.add_argument("note_contents", help="What your note is about")
+args = parser.parse_args()
+# ic(args)
+# ic(args.note_type)
+# = Path.home() / "Documents" / "konote_notes" / "notes.txt"
 
 
-with open("notes.json", "w+") as fout:
-    ujson.dump(notes, fout)
+#
+# with open("notes.json", "w+") as fout:
+#     ujson.dump(notes, fout)
