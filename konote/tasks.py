3a86427c09fe5b3319ac4443cee1d1d4e30dec20
KarthikB-dev <saberlight35@gmail.com>
2021-07-10 10:33:48 -0700

    Make each tasks a dict

--- konote/tasks.py
+++ konote/tasks.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python3
 from pathlib import Path
 import argparse
-import yaml
+
+# import yaml
+
 # from icecream import ic
 
 # import ujson
+notes = {}
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "note_type", type=int, help="1 for quick todo, 2 for project, 3 for long term goal"
+    "note_type", help="qt for quick todo, p for project, ltg for long term goal"
 )
-parser.add_argument("note_contents", help="What your note is about")
+parser.add_argument("note_contents", help="What your note is about, in quotes")
 args = parser.parse_args()
-# ic(args)
-# ic(args.note_type)
+
+notes[args.note_type] = {args.note_contents}
+# ic(notes)
 # = Path.home() / "Documents" / "konote_notes" / "notes.txt"
 
 
