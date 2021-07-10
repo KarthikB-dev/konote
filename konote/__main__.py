393d6ae09234187bb2d9f234c4fcdd1647b22b40
KarthikB-dev <saberlight35@gmail.com>
2021-07-10 10:39:45 -0700

    Style: Now adheres to PEP8

--- konote/__main__.py
+++ konote/__main__.py
@@ -1,10 +1,9 @@
 import re
 import sys
 
-#thank you to r2dev2 and kento nishi for this code
+# thank you to r2dev2 and kento nishi for this code
 from konote import main
 
 if __name__ == "__main__":
     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
     sys.exit(main())
-
