fb990e1a0f5a2610558f45072026393674f94dbb
KarthikB-dev <saberlight35@gmail.com>
2021-07-11 12:00:10 -0700

    Add support for reading and writing to yaml

--- konote/tasks.py
+++ konote/tasks.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python3
-from pathlib import Path
+# from pathlib import Path
 import argparse
 
-# import yaml
+import yaml
 
 # from icecream import ic
 
-# import ujson
-notes = {}
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "note_type", help="qt for quick todo, p for project, ltg for long term goal"
-)
-parser.add_argument("note_contents", help="What your note is about, in quotes")
-args = parser.parse_args()
-
-notes[args.note_type] = {args.note_contents}
-# ic(notes)
-# = Path.home() / "Documents" / "konote_notes" / "notes.txt"
+tasks = {}
+args = None
 
 
-#
-# with open("notes.json", "w+") as fout:
-#     ujson.dump(notes, fout)
+def read_tasks():
+    with open("Tasks.yaml", "r") as fin:
+        global tasks
+        tasks = yaml.safe_load(fin)
+
+
+def task_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "task_type", help="qt for quick todo, pr for project, ltg for long term goal"
+    )
+    parser.add_argument("task_contents", help="What your task is about, in quotes")
+    global args
+    args = parser.parse_args()
+
+
+def write_tasks():
+    valid_task_types = ["qt", "pr", "ltg"]
+    if args.task_type in valid_task_types:
+        tasks[args.task_type].append(args.task_contents)
+        with open("Tasks.yaml", "w") as fout:
+            yaml.dump(tasks, fout)
+
+
+def main():
+    read_tasks()
+    task_input()
+    write_tasks()
+    # ic(tasks)
+
+
+if __name__ == "__main__":
+    main()
