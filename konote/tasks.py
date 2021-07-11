661969dfd43f9ea41dcb289b2970264816df9863
KarthikB-dev <saberlight35@gmail.com>
2021-07-11 15:36:31 -0700

    Makes copy instead of appending

--- konote/tasks.py
+++ konote/tasks.py
@@ -5,12 +5,16 @@ import argparse
 import yaml
 
 # from icecream import ic
+import copy
 
 
 def read_tasks(tasks):
     # TODO store Tasks.yaml in home directory
     with open("Tasks.yaml", "r") as fin:
         tasks = yaml.safe_load(fin)
+    # if the program has been run for the first time
+    if tasks == None:
+        tasks = {"qt": [], "pr": [], "ltg": []}
     return tasks
 
 
@@ -20,27 +24,25 @@ def task_input():
         "task_type", help="qt for quick todo, pr for project, ltg for long term goal"
     )
     parser.add_argument("task_contents", help="What your task is about, in quotes")
-    global args
     args = parser.parse_args()
     return args
 
 
-def write_tasks():
+def write_tasks(args, tasks):
     valid_task_types = ["qt", "pr", "ltg"]
     if args.task_type in valid_task_types:
-        tasks[args.task_type].append(args.task_contents)
+        new_tasks = copy.deepcopy(tasks)
+        new_tasks[args.task_type] = new_tasks[args.task_type] + [args.task_contents]
         with open("Tasks.yaml", "w") as fout:
-            yaml.dump(tasks, fout)
+            yaml.dump(new_tasks, fout)
     else:
         print("error: invalid task type")
 
 
 def main():
-    tasks = {}
-    tasks = read_tasks(tasks)
+    tasks = read_tasks({})
     args = task_input()
-    write_tasks(args)
-    # ic(tasks)
+    write_tasks(args, tasks)
 
 
 if __name__ == "__main__":
