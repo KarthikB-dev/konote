b8f45ce17802cb6b0fe1c2e3ca23c96b62856d8d
KarthikB-dev <saberlight35@gmail.com>
2021-07-12 21:03:47 -0700

    Run konot's main

--- konote/__init__.py
+++ konote/__init__.py
@@ -1,11 +1,9 @@
 import argparse
 import sys
 from pathlib import Path
-
+from konote.tasks import *
 # thank you to r2dev2 for this program
 def main():
-    pass
+    print("Welcome to konote *hacker noises")
+    konote.tasks.main()
     # TODO complete
-
-
-main()
