d0fa0f178d9c247108380193f044b5bfeee3f2bd
KarthikB-dev <saberlight35@gmail.com>
2021-07-12 23:07:28 -0700

    Style: fix spacing issues

--- konote/__init__.py
+++ konote/__init__.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 from pathlib import Path
 from konote.tasks import *
+
 # thank you to r2dev2 for this program
 # def main():
 #     print("Welcome to konote *hacker noises*")
