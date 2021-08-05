54a664c77fb3f585b4988d0bfc1b8c279fb71e7e
KarthikB-dev <saberlight35@gmail.com>
2021-08-05 09:04:09 -0700

    Add function to get date of tomorrow

--- konote/freq.py
+++ konote/freq.py
@@ -2,10 +2,14 @@ from datetime import datetime, timedelta, date
 
 def today():
     today = date.today()
-    print(today)
+    return today
+
+def tmrw(today):
+    tmrw = today + timedelta(days=1)
+    print(tmrw)
 
 def main():
-    today()
+    tmrw(today())
 
 if __name__ == "__main__":
     main()
