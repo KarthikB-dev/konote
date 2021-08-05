a4ac1271cbfdac80b8ad07d20d0ce9e9b86a96c8
KarthikB-dev <saberlight35@gmail.com>
2021-08-05 09:07:23 -0700

    Add function to get date of number of days from now

--- konote/freq.py
+++ konote/freq.py
@@ -1,15 +1,26 @@
 from datetime import datetime, timedelta, date
 
-def today():
+
+def get_today():
     today = date.today()
     return today
 
-def tmrw(today):
+
+def get_tmrw(today):
     tmrw = today + timedelta(days=1)
-    print(tmrw)
+    return tmrw
+
+
+def get_days_from_now(today, days_from_today):
+    new_date = today + timedelta(days=days_from_today)
+    return new_date
+
 
 def main():
-    tmrw(today())
+    today = get_today()
+    five_days = get_days_from_now(today, 5)
+    print(five_days)
+
 
 if __name__ == "__main__":
     main()
