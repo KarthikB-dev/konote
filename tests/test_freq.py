ebe1fa18c93012017cd1e92198c980f00fd05180
KarthikB-dev <saberlight35@gmail.com>
2021-08-10 18:22:57 -0700

    Add test to get today's date

--- tests/test_freq.py
+++ tests/test_freq.py
@@ -1,1 +1,12 @@
 from konote.freq import *
+from icecream import ic
+
+def test_tmrw():
+    today = date.today()
+    ic(today)
+
+def main():
+    test_tmrw()
+
+if __name__ == "__main__":
+    main()
