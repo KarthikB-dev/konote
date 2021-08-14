badad2d51cef9386652e805043fccf6488cf4199
KarthikB-dev <saberlight35@gmail.com>
2021-08-13 23:08:21 -0700

    Remove icecream

--- tests/test_freq.py
+++ tests/test_freq.py
@@ -1,23 +1,19 @@
 from konote.freq import *
-from icecream import ic
 
 
 def test_tmrw():
     today = date.today()
-    ic(today)
 
 
 def test_days_between_dates():
     d1 = date.fromisoformat("2020-01-01")
     d2 = date.fromisoformat("2020-01-31")
     days_between = days_between_dates(d1, d2)
-    ic(days_between)
     assert days_between == 30
 
 
 def test_read_json():
     in_dict = read_json()
-    ic(in_dict)
     return in_dict
 
 
@@ -30,7 +26,6 @@ def test_edit_json():
     d = read_json()
     d["hi"] = "bye bye"
     write_to_json(d)
-    ic(read_json())
 
 
 def main():
