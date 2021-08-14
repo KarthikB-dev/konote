5ccaa047b57f91b96e0fc9f10085f3d8e5cc987c
KarthikB-dev <saberlight35@gmail.com>
2021-08-14 08:27:25 -0700

    Add tests of task status

--- tests/test_freq.py
+++ tests/test_freq.py
@@ -3,15 +3,28 @@ from konote.freq import *
 
 def test_read_json():
     in_dict = read_json()
-    return in_dict
+    assert in_dict is not None
 
 
 def test_write_json():
-    write_to_json({"hi": "bye"})
-    assert read_json() == {"hi": "bye"}
+    write_to_json({"Finish konote": "TODO"})
+    assert read_json() == {"Finish konote": "TODO"}
 
 
 def test_edit_json():
+    write_to_json({"Test edit": "May not work"})
     d = read_json()
-    d["hi"] = "bye bye"
+    d["Test edit"] = "Works"
     write_to_json(d)
+    assert read_json() == {"Test edit": "Works"}
+
+
+def test_change_task_status():
+    write_to_json({"Finish konote": None})
+    d = read_json()
+    d = todo_task(d, "Finish konote")
+    assert d == {"Finish konote": "TODO"}
+    d = in_progress_task(d, "Finish konote")
+    assert d == {"Finish konote": "IN_PROGRESS"}
+    d = complete_task(d, "Finish konote")
+    assert d == {"Finish konote": "DONE"}
