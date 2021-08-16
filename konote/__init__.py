55cf5ba80c2dab449ac9bdc6f10178d548863953
KarthikB-dev <saberlight35@gmail.com>
2021-08-16 11:04:40 -0700

    Add dumping initial date as part of initialization

--- konote/__init__.py
+++ konote/__init__.py
@@ -2,7 +2,16 @@ import argparse
 import sys
 from pathlib import Path
 from konote.tasks import *
+from datetime import datetime, timedelta, date
+import ujson
+
 
 # thank you to r2dev2 for this program
-# def main():
-#     print("Welcome to konote *hacker noises*")
+def main():
+    print("Welcome to konote *hacker noises*")
+    # initializes freq json
+    json_path = Path.home() / "konote_files" / "freq.json"
+    # TODO test
+    init_dict = {"init_date": date.today()}
+    with open(json_path, "w") as fout:
+        ujson.dump(init_dict, fout)
