cfa64e261fe291401d78caceb69200d7c0b436d5
KarthikB-dev <saberlight35@gmail.com>
2021-08-17 12:12:50 -0700

    Fix: did not correctly initialize json file

--- konote/__init__.py
+++ konote/__init__.py
@@ -11,8 +11,8 @@ def main():
     print("Welcome to konote *hacker noises*")
     # initializes freq json
     json_path = Path.home() / "konote_files" / "freq.json"
-    # TODO test
-    if json_path.is_file():
+    if not json_path.is_file():
         init_dict = {"init_date": date.isoformat(date.today())}
+        init_dict['todos'] = None
         with open(json_path, "w") as fout:
             ujson.dump(init_dict, fout)
